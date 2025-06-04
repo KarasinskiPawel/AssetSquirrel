9e8844782fe36dd0af3c65c1c69dd4f4d18699c7
Karasiński Paweł <pawelkarasinski1981@o2.pl>
2025-06-04 15:12:40 +0200

    2025-06-04

---
+++ AssetSquirrel.WebApp/Data/AssetSquirrelUserIdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace AssetSquirrel.WebApp.Data
+{
+    public class AssetSquirrelUserIdentityContext(DbContextOptions<AssetSquirrelUserIdentityContext> options) : IdentityDbContext<IdentityUser>(options)
+    {
+    }
+}
--- AssetSquirrel.WebApp/Program.cs
+++ AssetSquirrel.WebApp/Program.cs
@@ -1,20 +1,62 @@
 using AssetSquirrel.UseCases.Extensions;
-using AssetSquirrel.UseCases.Locations.Interfaces;
 using AssetSquirrel.UseCases.Locations;
-using AssetSquirrel.WebApp.Components;
-using AssetsSquirrel.Plugins.EFCoreSqlServer;
-using Microsoft.EntityFrameworkCore;
+using AssetSquirrel.UseCases.Locations.Interfaces;
 using AssetSquirrel.UseCases.PluginInterfaces;
-using AssetsSquirrel.Plugins.EFCoreSqlServer.Repositories;
+using AssetSquirrel.WebApp.Components;
+using AssetSquirrel.WebApp.Data;
 using AssetSquirrel.WebApp.Extensions;
+using AssetsSquirrel.Plugins.EFCoreSqlServer;
+using AssetsSquirrel.Plugins.EFCoreSqlServer.Repositories;
+using BlazorWebApp.Authentication.Components.Account;
+using BlazorWebApp.Authentication.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("AssetsSquirrelAccountsDB") ?? throw new InvalidOperationException("Connection string 'AssetsSquirrelAccountsDB' not found.");
+builder.Services.AddDbContext<AssetSquirrelUserIdentityContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AssetSquirrelUserIdentityContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+if (builder.Environment.IsEnvironment("Testing"))
+{
+
+}
+else
+{
+
+}
 
 // Add services to the container.
 builder.Services.AddDbContextFactory<AssetsSquirrelContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AssetsSquirrelDB")
-        ,sql => sql.MigrationsAssembly("AssetsSquirrel.Plugins.EFCoreSqlServer"));
+        , sql => sql.MigrationsAssembly("AssetsSquirrel.Plugins.EFCoreSqlServer"));
 });
 
 builder.Services.AddRazorComponents()
@@ -44,6 +86,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseMigrationsEndPoint();
+}
 
 app.UseHttpsRedirection();
 
@@ -52,5 +98,8 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity system / Account Razor Components.
+app.MapAdditionalIdentityEndpoints();
 
 app.Run();
