a930035d42909fcf5ecd8d8f51f0b3e035439555
Karasiński Paweł <pawelkarasinski1981@o2.pl>
2026-07-10 10:13:06 +0200

    Loguj do tabeli Errors nieudana wysylke maila w ResendEmailConfirmation
    ResendEmailConfirmation.razor lapal wyjatek z EmailSender.SendConfirmationLinkAsync
    i pokazywal komunikat uzytkownikowi, ale sam nie zapisywal go do tabeli Errors -
    dodano AddErrorAsync w tym catch, drobny formatting w SmtpEmailSender.cs.

--- AssetSquirrelAuthorize.WebApp/Components/Account/SmtpEmailSender.cs
+++ AssetSquirrelAuthorize.WebApp/Components/Account/SmtpEmailSender.cs
@@ -13,6 +13,7 @@ namespace AssetSquirrelAuthorize.WebApp.Components.Account
 
         public SmtpEmailSender(
             IConfiguration configuration,
+            
             IErrorsRepository errorsRepository
             )
         {
